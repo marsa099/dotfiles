a8213aae962ee526c06030a50dc41a3e63b94bde
Martin Sandberg <marsa099@gmail.com>
2024-07-31 17:54:07 +0200

    Adds install-tmux script

---
+++ .install/install-tmux.sh
@@ -0,0 +1,12 @@
+## Tmux, https://github.com/tmux/tmux
+sudo pacman -Syu tmux 
+
+## Tmux Plugin Manager, https://github.com/tmux-plugins/tpm
+TMUX_PLUGINS=~/.tmux/plugins/tpm
+mkdir -p $TMUX_PLUGINS
+if [[ -d "$TMUX_PLUGINS/.git" ]]
+then
+	git -C $TMUX_PLUGINS pull
+else
+	git clone https://github.com/tmux-plugins/tpm "$TMUX_PLUGINS" --depth=1
+fi
