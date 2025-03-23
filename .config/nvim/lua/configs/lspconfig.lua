d4c412defbc929e97b124f75512f7c6fa967d741
Martin Sandberg <martin@koderiet.dev>
2025-03-23 07:34:12 +0100

    Adds omnisharp lsp + omnisharp extended as client handler for gd/gi

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -2,6 +2,7 @@
 require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
+local omnisharp_ext = require("omnisharp_extended")
 
 -- EXAMPLE
 local servers = { "html", "cssls" }
@@ -22,3 +23,16 @@ end
 --   on_init = nvlsp.on_init,
 --   capabilities = nvlsp.capabilities,
 -- }
+
+lspconfig.omnisharp.setup {
+  cmd = { "omnisharp", "--languageserver", "--hostPID", tostring(vim.fn.getpid()) },
+  on_attach = function(client, bufnr)
+    nvlsp.on_attach(client, bufnr)
+    -- Patcha handlers
+    client.handlers["textDocument/definition"] = omnisharp_ext.handler
+    client.handlers["textDocument/typeDefinition"] = omnisharp_ext.handler
+    client.handlers["textDocument/implementation"] = omnisharp_ext.handler
+  end,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+}
