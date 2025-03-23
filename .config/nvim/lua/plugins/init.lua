35be5b517f4e555a5b135ec8ce3f6acbe4e31e12
Martin Sandberg <martin@koderiet.dev>
2025-03-23 07:37:10 +0100

    Adds c_sharp in ensure_installed for nvim-treesitter + adds omnisharp-extended-lsp in list of plugins

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -13,13 +13,18 @@ return {
     end,
   },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+   {
+   	"nvim-treesitter/nvim-treesitter",
+   	opts = {
+   		ensure_installed = {
+   			"vim", "lua", "vimdoc",
+        "html", "css", "c_sharp"
+   		},
+   	},
+   },
+  {
+    "Hoffs/omnisharp-extended-lsp.nvim",
+    lazy = true,
+    ft = { "cs" },
+  },
 }
