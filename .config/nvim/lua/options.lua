30b60c96a6d899315cb160dc321f760cb91e2a61
Martin Sandberg <martin@koderiet.dev>
2025-03-27 20:58:44 +0100

    Adds scrolloff to keep 10 lines above/below cursor when scrolling

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -2,6 +2,11 @@ require "nvchad.options"
 
 -- add yours here!
 
+-- Enable relative line numbers
 vim.o.relativenumber = true
+
+-- Minimum 10 lines below/above cursor when scrolling
+vim.o.scrolloff = 10
+
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
