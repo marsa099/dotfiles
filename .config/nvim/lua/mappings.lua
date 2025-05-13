0256d94da2bcc47091ef284fbd9221c0beec797c
Martin Sandberg <martin@koderiet.dev>
2025-05-13 10:07:48 +0200

    Reverts magic git checkout command, installs ts formatters, adds copilot auto complete/chat for nvim, adds a lot of keybindings in nvim

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -27,3 +27,55 @@ map("n", "<leader>D", function()
   require("omnisharp_extended").telescope_lsp_references()
 end, { noremap = true })
 map("n", "gi", require("omnisharp_extended").telescope_lsp_implementation, { noremap = true })
+
+map(
+  "n",
+  "<leader>dp",
+  "<cmd>%delete _<bar>0put +<CR>",
+  { desc = "Replace buffer with clipboard", noremap = true, silent = true }
+)
+
+map("n", "<leader>%y", function()
+  -- Hämta filnamn utan path
+  local fname = vim.fn.expand "%:t"
+  -- Hämta alla rader i buffern
+  local buf = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+  -- Bygg en lista med fname + tom rad + själva innehållet
+  local content = { fname, "" }
+  for _, line in ipairs(buf) do
+    table.insert(content, line)
+  end
+  -- Skriv till system-clipboard
+  vim.fn.setreg("+", content)
+  -- Valfritt meddelande:
+  vim.notify("Kopierade: " .. fname, vim.log.levels.INFO)
+end, {
+  desc = "Insert buffer to clipboard",
+  noremap = true,
+  silent = true,
+})
+
+local function yank_diagnostics()
+  -- 1) sätt location-list utan att öppna
+  vim.diagnostic.setloclist { open = false }
+  -- 2) öppna, yanka hela fönstret till +-registret, stäng
+  vim.cmd "lopen"
+  vim.cmd "%yank +"
+  vim.cmd "lclose"
+  -- valfri notis
+  vim.notify("Diagnostics yanked to clipboard", vim.log.levels.INFO)
+end
+
+map("n", "<leader>cd", yank_diagnostics, { desc = "Yank diagnostics to clipboard", noremap = true, silent = true })
+
+map("n", "<leader>gc", "<cmd>CopilotChatToggle<CR>", {
+  noremap = true,
+  silent = true,
+  desc = "Toggle Copilot Chat",
+})
+
+map("n", "<leader>ga", "<cmd>CopilotChatAgents<CR>", {
+  noremap = true,
+  silent = true,
+  desc = "Select Copilot chat agent",
+})
