72013f642d8ba5c2639d223d67108158a75d438e
Martin Sandberg <7248231+marsa099@users.noreply.github.com>
2025-10-16 21:42:14 +0200

    Nukes nvim conf

--- .config/nvim/lua/custom/autocmds.lua
+++
@@ -1,7 +0,0 @@
--- q closes help buffer without need to type : first 
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "help",
-  callback = function()
-    vim.keymap.set("n", "q", "<cmd>quit<CR>", { buffer = true })
-  end,
-})
