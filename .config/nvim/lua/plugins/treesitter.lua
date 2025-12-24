da7b44efd1c523df510d49855ecb705c7c31749a
Martin Sandberg <7248231+marsa099@users.noreply.github.com>
2025-12-24 13:15:07 +0100

    Add treesitter-textobjects for method navigation

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,6 +3,9 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		lazy = false,
 		build = ":TSUpdate",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+		},
 		config = function()
 			require("nvim-treesitter.configs").setup({
 				ensure_installed = {
@@ -25,6 +28,24 @@ return {
 				},
 				indent = {
 					enable = true,
+				},
+				textobjects = {
+					move = {
+						enable = true,
+						set_jumps = true,
+						goto_next_start = {
+							["]m"] = "@function.outer",
+						},
+						goto_next_end = {
+							["]M"] = "@function.outer",
+						},
+						goto_previous_start = {
+							["[m"] = "@function.outer",
+						},
+						goto_previous_end = {
+							["[M"] = "@function.outer",
+						},
+					},
 				},
 			})
 		end,
