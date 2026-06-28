495d4e05d07d50a028748cb255062c1b201f9b42
Martin Sandberg <7248231+marsa099@users.noreply.github.com>
2026-06-28 06:04:10 +0200

    Add line numbers to kitty-scrollback nvim buffer

--- .config/nvim/lua/plugins/kitty-scrollback.lua
+++ .config/nvim/lua/plugins/kitty-scrollback.lua
@@ -6,5 +6,12 @@ return {
 	event = { "User KittyScrollbackLaunch" },
 	config = function()
 		require("kitty-scrollback").setup()
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "kitty-scrollback",
+			callback = function()
+				vim.wo.number = true
+				vim.wo.relativenumber = true
+			end,
+		})
 	end,
 }
