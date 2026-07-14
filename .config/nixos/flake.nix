189f9b21c9e3e88bc84167a6de7e2a3c49ec4ed5
Martin Sandberg <7248231+marsa099@users.noreply.github.com>
2026-07-14 14:12:22 +0200

    Upgrade to NixOS 26.05 (kernel 6.18.38); fix nodePackages removal

--- .config/nixos/flake.nix
+++ .config/nixos/flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     # Community flake for Helium browser (downloads upstream prebuilt release).
     # Not in nixpkgs. Update with: nix flake update helium-browser
