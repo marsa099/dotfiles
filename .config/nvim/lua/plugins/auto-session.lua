ff1fab178e44097c600c0af45dbd86518e39ff15
Martin Sandberg <7248231+marsa099@users.noreply.github.com>
2026-07-14 11:15:35 +0200

    Excludes terminal buffers from nvim auto-sessions

--- .config/nvim/lua/plugins/auto-session.lua
+++ .config/nvim/lua/plugins/auto-session.lua
@@ -1,6 +1,12 @@
 return {
 	{
 		"rmagatti/auto-session",
+		init = function()
+			-- Don't save terminal buffers into sessions: a restored terminal can't
+			-- resurrect its old process, it just re-runs the command (which for
+			-- kitty-scrollback.nvim buffers meant piles of dead "command not found" tabs).
+			vim.opt.sessionoptions:remove("terminal")
+		end,
 		opts = {
 			auto_session_suppress_dirs = { "~/", "~/Downloads", "/" },
 		},
